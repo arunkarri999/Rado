5ce875979eb7461e69add517b664438e1d84e633
Sathvik777 <katam.sathvik@gmail.com>
2018-02-19 06:27:41 +0100

    models for auth

---
+++ backend/radoBackend/models/Login.cs
@@ -0,0 +1,7 @@
+namespace radoBackend.models {
+    public class Login {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Passowrd { get; set; }
+    }
+}
---
+++ backend/radoBackend/models/SignUp.cs
@@ -0,0 +1,8 @@
+namespace radoBackend.models {
+    public class SignUp {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Passowrd { get; set; }
+        public string AuthToken{ get; set; }
+    }
+}
